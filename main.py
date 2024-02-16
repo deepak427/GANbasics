6d35d605f96bb5566e68cc31d04796d0e7c0feb9
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-16 21:35:45 +0530

    Built Generator sequential model

--- main.py
+++ main.py
@@ -28,3 +28,60 @@ for idx in range(4):
     # Appending the image label as the plot title 
     ax[idx].title.set_text(sample['label'])
 
+# Scale and return images only
+def scale_images(data):
+    image = data['image']
+    return image / 255
+
+# Running the dataset through the scale_images preprocessing step
+ds = ds.map(scale_images) 
+# Cache the dataset for that batch 
+ds = ds.cache()
+# Shuffle it up 
+ds = ds.shuffle(60000)
+# Batch into 128 images per sample
+ds = ds.batch(128)
+# Reduces the likelihood of bottlenecking 
+ds = ds.prefetch(64)
+
+# Build Neural Network
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D
+
+# Build Generator
+
+def build_generator(): 
+    model = Sequential()
+    
+    # Takes in random values and reshapes it to 7x7x128
+    # Beginnings of a generated image
+    model.add(Dense(7*7*128, input_dim=128))
+    model.add(LeakyReLU(0.2))
+    model.add(Reshape((7,7,128)))
+    
+    # Upsampling block 1 
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, 5, padding='same'))
+    model.add(LeakyReLU(0.2))
+    
+    # Upsampling block 2 
+    model.add(UpSampling2D())
+    model.add(Conv2D(128, 5, padding='same'))
+    model.add(LeakyReLU(0.2))
+    
+    # Convolutional block 1
+    model.add(Conv2D(128, 4, padding='same'))
+    model.add(LeakyReLU(0.2))
+    
+    # Convolutional block 2
+    model.add(Conv2D(128, 4, padding='same'))
+    model.add(LeakyReLU(0.2))
+    
+    # Conv layer to get to one channel
+    model.add(Conv2D(1, 4, padding='same', activation='sigmoid'))
+    
+    return model
+
+test_model = build_generator()
+test_model.summary()
